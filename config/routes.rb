fb9cce1815bad4deb36c9eaaf31b56a7d85a9b09
CronusUI <murray@cronusui.com>
2016-09-06 01:08:48 +0000

    Created new theme, created sessions, restricted UI based on sessions

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
   
   
   resources :users
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
